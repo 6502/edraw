f7288bacd78dcf519e02c3059c1880e0300d5adb
Andrea Griffini <griff@tin.it>
2019-02-15 21:48:25 +0100

    Added curve arrow support

--- bez2.js
+++ bez2.js
@@ -1,15 +1,16 @@
 class Bez2 {
-    constructor(a, b, c, style) {
+    constructor(a, b, c, flags, style) {
         this.a = a;
         this.b = b;
         this.c = c;
+        this.flags = flags;
         this.style = style;
     }
     editor() {
         return new Bez2Editor(this);
     }
     clone() {
-        return new Bez2(this.a, this.b, this.c, clone(this.style));
+        return new Bez2(this.a, this.b, this.c, this.flags, clone(this.style));
     }
     setStyle(f, val) {
         ur_set(this.style, f, val);
@@ -18,6 +19,17 @@ class Bez2 {
         let b = bez2Interp(this.a, this.b, this.c);
         drawBez2(ctx, this.a, b, this.c,
                  this.style.stroke, this.style.width);
+        let arrow = (a, b)=>{
+            let dx = b.x - a.x, dy = b.y - a.y,
+                L = (dx*dx + dy*dy)**0.5,
+                wx = dx/L*50, wy = dy/L*50;
+            fillPolygon(ctx, [{x:b.x+wx/4, y:b.y+wy/4},
+                              {x:b.x-wx+wy/3, y:b.y-wy-wx/3},
+                              {x:b.x-wx-wy/3, y:b.y-wy+wx/3}],
+                       this.style.stroke);
+        };
+        if (this.flags & 1) arrow(b, this.a);
+        if (this.flags & 2) arrow(b, this.c);
     }
     hit(x, y, b) {
         let p = rmap(x, y)
@@ -29,7 +41,12 @@ class Bez2 {
                 if (b === 0) {
                     drag(this);
                 } else {
-                    editPopup(this, x, y);
+                    editPopup(this, x, y,
+                              "Curve",
+                              {text:"{arrow_forward} Toggle end arrow", action:()=>{this.editor().arrow(1)}},
+                              {text:"{arrow_back} Toggle start arrow", action:()=>{this.editor().arrow(-1)}},
+                             );
+
                 }
                 return true;
             }
@@ -61,6 +78,18 @@ class Bez2Editor {
     draw(ctx) {
         dot(ctx, this.e.a); dot(ctx, this.e.b); dot(ctx, this.e.c);
     }
+    arrow(d) {
+        if (d === -1) {
+            ur_begin("Bez2 curve start arrow toggle");
+            ur_set(this.e, "flags", this.e.flags ^ 1);
+            ur_end();
+        } else if (d === 1) {
+            ur_begin("Bez2 curve end arrow toggle");
+            ur_set(this.e, "flags", this.e.flags ^ 2);
+            ur_end();
+        }
+        invalidate();
+    }
     hit(x, y, b) {
         let p = rmap(x, y);
         if (b === 0) {
@@ -88,7 +117,7 @@ function draw_curve() {
         },
         hit(x, y, b) {
             let p = rmap(x, y);
-            let c = new Bez2(p, p, p, {stroke:"#000", width:8});
+            let c = new Bez2(p, p, p, 0, {stroke:"#000", width:8});
             ur_begin("Curve draw");
             ur_add({undo(){ entities.pop(); }, redo(){ entities.push(c); }});
             ur_end();
